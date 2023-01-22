ef7a8ad3efd722c38e8cb46619cef41b4e10eac6
ranforrestren <ranforrestren@gmail.com>
2023-01-22 16:41:56 -0500

    Add anAdd an embarassing anime gif depending on weather in location

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const searchButton = document.querySelector("#searchButton")
 
 async function fetchWeatherData(location) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=16962f48bc81e37b5f8eae8d6802cd48&units=metric`, {mode: `cors`});
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=16962f48bc81e37b5f8eae8d6802cd48&units=metric`, { mode: `cors` });
         const rawWeatherData = await response.json();
         console.log(rawWeatherData);
         const weatherData = weatherDataFactory(rawWeatherData);
         console.log(weatherData);
         injectWeatherData(weatherData);
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
@@ -34,12 +34,36 @@ function injectWeatherData(weatherData) {
     const feelslikeElem = document.querySelector("#feels_like");
     const humidityElem = document.querySelector("#humidity");
     const windElem = document.querySelector("#wind");
+    const background = document.querySelector('body');
     nameElem.textContent = weatherData.name;
-    weatherElem.textContent = "Weather: " + weatherData.weather[0].main;
     temperatureElem.textContent = Math.round(weatherData.temp) + "°C";
-    feelslikeElem.textContent = "Feels like: " + weatherData.feels_like;
-    humidityElem.textContent = "Humidity: " + weatherData.humidity;
-    windElem.textContent = "Wind: " + weatherData.wind;
+    weatherElem.textContent = weatherData.weather[0].description;
+    feelslikeElem.textContent = Math.round(weatherData.feels_like) + "°C";
+    humidityElem.textContent = weatherData.humidity + "%";
+    windElem.textContent = weatherData.wind + "m/s";
+    background.className = "";
+    switch (weatherData.weather[0].main) {
+        case "Thunderstorm":
+            background.classList.add("thunderstorm");
+            break;
+        case "Drizzle":
+            background.classList.add("drizzle");
+            break;
+        case "Rain":
+            background.classList.add("rain");
+            break;
+        case "Snow":
+            background.classList.add("snow");
+            break;
+        case "Clear":
+            background.classList.add("clear");
+            break;
+        case "Clouds":
+            background.classList.add("clouds");
+            break;
+        default:
+            background.classList.add("atmosphere");
+    }
 }
 
 fetchWeatherData("toronto");
